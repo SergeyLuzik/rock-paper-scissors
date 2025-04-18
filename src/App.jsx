183e07a4b9ad731be60d9ffc98a432737b748d7f
Sergey Luzik <luziksergey@gmail.com>
2025-04-18 06:27:03 +0300

    feat: adds mode reducer to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Greeting } from './components/Greeting';
 import { SwitchThemeButton } from './components/SwitchThemeButton';
 import { Game } from './components/Game';
+import { useDispatch, useSelector } from 'react-redux';
+import { switchMode } from './actions/mode';
 
 export const App = () => {
+  const mode = useSelector((state) => state.mode);
+  const dispatch = useDispatch();
+  const handlePlayClick = () => {
+    dispatch(switchMode());
+  };
   return (
     <div className="relative flex h-screen flex-col items-center justify-center">
       <SwitchThemeButton />
-      <Greeting />
-      <Game />
+      {mode === 'greeting' ? <Greeting onPlayClick={handlePlayClick} /> : <Game />}
     </div>
   );
 };
