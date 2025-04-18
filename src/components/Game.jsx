cfc1353c1c07a862038b3307511a60d2941da768
Sergey Luzik <luziksergey@gmail.com>
2025-04-18 09:34:14 +0300

    feat: adds score and winner reducers to the Game component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,32 +1,72 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { defineWinner, getComputerChoice, getResult } from '../services/game';
 import { Button } from './Button';
+import { computerScoreIncrease, playerScoreIncrease } from '../actions/score';
+import { setWinner } from '../actions/winner';
 
-export const Game = () => (
-  <div className="flex flex-col gap-8 text-center">
-    <h1 className="text-2xl text-secondary">Rock Paper Scissors</h1>
-    <button className="cursor-pointer text-base text-primary" type="button">
-      {'Reset the score'.toUpperCase()}
-    </button>
-    <div className="flex justify-between">
-      <div className="flex text-sm text-primary">
-        <p>{'player score:'.toUpperCase()}</p>
-        <output>0</output>
+export const Game = () => {
+  const score = useSelector((state) => state.score);
+  const dispatch = useDispatch();
+  const handleResetScore = () => {};
+  const winner = useSelector((state) => state.winner);
+  const handlePlayerChoice = (playerChoice) => {
+    const computerChoice = getComputerChoice();
+    dispatch(setWinner(defineWinner(playerChoice, computerChoice)));
+
+    if (winner === 'player') {
+      dispatch(playerScoreIncrease());
+    } else {
+      dispatch(computerScoreIncrease());
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-8 text-center">
+      <h1 className="text-2xl text-secondary">Rock Paper Scissors</h1>
+      <button
+        onClick={handleResetScore}
+        className="cursor-pointer text-base text-primary uppercase"
+        type="button">
+        Reset the score
+      </button>
+      <div className="flex justify-between uppercase">
+        <div className="flex text-sm text-primary">
+          <p>player score:</p>
+          <output>{score.player}</output>
+        </div>
+        <div className="flex text-sm text-primary">
+          <p>computer score:</p>
+          <output>{score.computer}</output>
+        </div>
       </div>
-      <div className="flex text-sm text-primary">
-        <p>{'computer score:'.toUpperCase()}</p>
-        <output>0</output>
+      {console.log(winner)}
+      <p className="text-4xl text-accent-second uppercase">{getResult(winner)}</p>
+      <span className="text-base text-primary">VS</span>
+      <div>
+        <p className="mb-6 text-sm font-medium text-third">
+          Choose your move, rock paper or scissors?
+        </p>
+        <div className="flex gap-12">
+          <Button
+            text="rock"
+            onClick={() => {
+              handlePlayerChoice('rock');
+            }}
+          />
+          <Button
+            text="paper"
+            onClick={() => {
+              handlePlayerChoice('paper');
+            }}
+          />
+          <Button
+            text="scissors"
+            onClick={() => {
+              handlePlayerChoice('scissors');
+            }}
+          />
+        </div>
       </div>
     </div>
-    <p className="text-4xl text-accent-second">YOU WON ! 🎉</p>
-    <span className="text-base text-primary">VS</span>
-    <div>
-      <p className="mb-6 text-sm font-medium text-third">
-        Choose your move, rock paper or scissors?
-      </p>
-      <div className="flex gap-12">
-        <Button text="rock" />
-        <Button text="paprer" />
-        <Button text="scissors" />
-      </div>
-    </div>
-  </div>
-);
+  );
+};
