0fbd706d541ec5cc7a190d9756b608217d4ce583
Sergey Luzik <luziksergey@gmail.com>
2025-04-18 09:32:56 +0300

    feat: adds score and winner reducers to the rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { theme } from './theme';
 import { mode } from './mode';
+import { score } from './score';
+import { winner } from './winner';
 
 export const rootReducer = combineReducers({
   theme,
-  mode
+  mode,
+  score,
+  winner
 });
