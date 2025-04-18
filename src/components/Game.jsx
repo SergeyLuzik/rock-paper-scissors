c68949245b905fe7594582328d3aea1baa2b33b0
Sergey Luzik <luziksergey@gmail.com>
2025-04-18 13:06:55 +0300

    ref: adds choice icons to the Game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,16 +3,24 @@ import { defineWinner, getComputerChoice, getResult } from '../services/game';
 import { Button } from './Button';
 import { computerScoreIncrease, playerScoreIncrease, resetScore } from '../actions/score';
 import { setWinner } from '../actions/winner';
+import { setComputerChoice, setPlayerChoice } from '../actions/choices';
+
+import RockIcon from '../assets/rock.svg?react';
+import PaperIcon from '../assets/paper.svg?react';
+import ScissorsIcon from '../assets/scissors.svg?react';
 
 export const Game = () => {
   const score = useSelector((state) => state.score);
+  const choices = useSelector((state) => state.choices);
   const dispatch = useDispatch();
   const handleResetScore = () => {
     dispatch(resetScore());
   };
   const winner = useSelector((state) => state.winner);
   const handlePlayerChoice = (playerChoice) => {
+    dispatch(setPlayerChoice(playerChoice));
     const computerChoice = getComputerChoice();
+    dispatch(setComputerChoice(computerChoice));
     dispatch(setWinner(defineWinner(playerChoice, computerChoice)));
 
     if (winner === 'player') {
@@ -22,8 +30,14 @@ export const Game = () => {
     }
   };
 
+  const icons = {
+    rock: <RockIcon />,
+    paper: <PaperIcon />,
+    scissors: <ScissorsIcon />
+  };
+
   return (
-    <div className="flex flex-col gap-8 text-center">
+    <div className="flex flex-col items-center gap-8 text-center">
       <h1 className="text-2xl text-secondary">Rock Paper Scissors</h1>
       <button
         onClick={handleResetScore}
@@ -41,9 +55,12 @@ export const Game = () => {
           <output>{score.computer}</output>
         </div>
       </div>
-      {console.log(winner)}
       <p className="text-4xl text-accent-second uppercase">{getResult(winner)}</p>
-      <span className="text-base text-primary">VS</span>
+      <div className="flex h-31.5 w-108.5 items-center justify-between text-primary">
+        <div className="size-31.5">{choices.player && icons[choices.player]}</div>
+        <span className="text-base uppercase">vs</span>
+        <div className="size-31.5">{choices.computer && icons[choices.computer]}</div>
+      </div>
       <div>
         <p className="mb-6 text-sm font-medium text-third">
           Choose your move, rock paper or scissors?
