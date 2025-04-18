2972776fe38ef48044d0614bc5d4e6d67f4c7d3f
Sergey Luzik <luziksergey@gmail.com>
2025-04-18 06:27:28 +0300

    feat: adds theme reducer to the SwitchThemeButton

--- src/components/SwitchThemeButton.jsx
+++ src/components/SwitchThemeButton.jsx
@@ -1,10 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { switchTheme } from '../actions/theme';
+// import lightModeIcon from '../assets/light-mode.svg';
+// import darkModeIcon from '../assets/dark-mode.svg';
+
 export const SwitchThemeButton = () => {
+  const theme = useSelector((state) => state.theme);
+  const themeIcon =
+    theme === 'light' ? 'after:bg-[image:var(--light-icon)]' : 'after:bg-[image:var(--dark-icon)]';
+  const dispatch = useDispatch();
   const handleThemeSwitch = () => {
+    dispatch(switchTheme());
     document.documentElement.classList.toggle('dark');
   };
   return (
     <button
-      className="absolute top-0 right-0 size-24 cursor-pointer bg-accent"
+      className={`absolute top-0 right-0 size-24 cursor-pointer after:block after:size-24 ${themeIcon}`}
       type="button"
       onClick={handleThemeSwitch}></button>
   );
