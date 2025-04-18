3a21a824a7cc52ffd60aa3b82077489d9d3e838a
Sergey Luzik <luziksergey@gmail.com>
2025-04-18 06:21:11 +0300

    feat: adds onClic to the Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,10 @@ const SIZES = {
   base: 'w-42.5 h-15 text-lg',
   large: 'w-42.5 h-22.5 text-3xl'
 };
-export const Button = ({ text, size = 'base' }) => {
+export const Button = ({ text, size = 'base', onClick }) => {
   return (
     <button
+      onClick={onClick}
       className={`flex cursor-pointer items-center justify-center rounded-md bg-accent text-white hover:bg-accent-second ${SIZES[size]}`}
       type="button">
       {text.toUpperCase()}
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,8 +1,8 @@
 import { Button } from './Button';
 
-export const Greeting = () => (
+export const Greeting = ({ onPlayClick }) => (
   <div className="flex items-center gap-8">
-    <Button text="play" size="large" />
+    <Button text="play" size="large" onClick={onPlayClick} />
     <h1 className="text-3xl leading-[1.215] text-secondary">
       Rock Paper Scissors <br />
       The game
